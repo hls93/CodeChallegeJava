13c14809394f06322d1c36c07807f03111717bd7
hls93 <hlshaw3@ncsu.edu>
2017-12-04 12:44:31 -0500

    Url is connected, and am able to parse out the text. Have created a list to contain all the words and their frequencies

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,12 +7,42 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-    Document doc = Jsoup.connect("https://www.bettycrocker.com/recipes/ultimate-chocolate-chip-cookies/77c14e03-d8b0-4844-846d-f19304f61c57").get();
-    String title = doc.title();
+     //using Jsoup, I am connecting to the url and parsing through the html
+     String webPage = "https://www.thespruce.com/worst-plants-for-allergies-4144982";
+
+     String html = Jsoup.connect(webPage).get().html();
+
+     Document doc = Jsoup.parse(String.valueOf(html));
+
+     //extracting the text from the body of the webpage
+     String text = doc.body().text();
+
+     //printing to make sure my extracting worked
+     //System.out.println(text);
+
+     //creating a new list to show the words on the webpage and their frequency
+     List<String> list = Arrays.asList(text.split(" "));
+
+     Set<String> topWords = new HashSet<String>(list);
+
+        for (String word: topWords ) {
+
+            if ()
+
+            System.out.println(word + ": " + Collections.frequency(list, word));
+
+        }
+
+
+
+
+
+
 }
 }
