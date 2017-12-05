4ea2d7eeeaac7f08bf7bd504abcae4b37a8d0fc9
hls93 <hlshaw3@ncsu.edu>
2017-12-05 12:38:38 -0500

    removing unneccesary code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,11 @@
 //package org.jsoup.examples;
 
 import org.jsoup.Jsoup;
-import org.jsoup.helper.Validate;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+
 
 import java.io.IOException;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.Comparator;
 
 public class Main {
 
@@ -22,15 +18,15 @@ public class Main {
 
      Document doc = Jsoup.parse(String.valueOf(html));
 
-     //extracting the text from the body of the webpage
+     //extracting the text from the body of the web-page
      String text = doc.body().text();
 
-     //creating a new list to show the words on the webpage and their frequency
+     //creating a new list to show the words on the web-page and their frequency
      List<String> list = Arrays.asList(text.split(" "));
      Collections.sort(list);
 
-     //creating a hashmap to store the word and its frequency
-     HashMap<String, Integer> map = new HashMap<String, Integer>();
+     //creating a hashMap to store the word and its frequency
+     HashMap<String, Integer> map = new HashMap<>();
 
      for (int i = 0; i < list.size();) {
          String s = list.get(i);
@@ -40,9 +36,8 @@ public class Main {
 
          i = list.lastIndexOf(s) +1;
      }
-        //System.out.println(map);
 
-
+    //now sorting the list by frequency and limiting it to print the top 25
         map.entrySet().stream()
                 .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
                 .limit(25)
